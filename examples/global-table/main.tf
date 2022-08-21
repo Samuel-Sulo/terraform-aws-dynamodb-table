2fca9c51352eed0fd00be80fc46f620f83efd05a
Samuel Sulo <samuelsulo0802@gmail.com>
2022-08-21 12:37:30 +0200

    refactor(examples): rename replicas to global-table, add additional settings

---
+++ examples/global-table/main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+module "config_db" {
+  source = "../../module"
+
+  name                           = "Configurations"
+  hash_key                       = "PK"
+  range_key                      = "SK"
+  stream_enabled                 = true
+  stream_view_type               = "NEW_AND_OLD_IMAGES"
+  server_side_encryption_enabled = true
+  point_in_time_recovery_enabled = true
+
+  attributes = {
+    PK = "S"
+    SK = "S"
+  }
+
+  replicas = [
+    {
+      region_name            = "eu-west-2"
+      point_in_time_recovery = true
+      propagate_tags         = true
+    }
+  ]
+
+  tags = {
+    Environment = "Development"
+  }
+}
